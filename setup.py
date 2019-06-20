61fddee838b49fb575dcc2040cb1a1100953464c
rcourivaud <r.courivaud@gmail.com>
2019-06-20 14:28:18 +0200

    clean setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ from distutils.core import setup, Extension
 with open('requirements.txt') as f:
 	requirements = f.read().splitlines()
 
-module = Extension('demo',
-                    define_macros=[('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
-                    include_dirs=['license_plate_extractor/darknet'],
-                    library_dirs=['license_plate_extractor/darknet'],
-                    sources=['license_plate_extractor/darknet/src/demo.c'])
-
 setup(
 	name='license_plate_extractor',
 	version='0.1',
@@ -22,5 +16,4 @@ setup(
 	author='Raphael Courivaud',
 	author_email='r.courivaud@gmail.com',
 	description='',
-	ext_modules = [module]
 )
